6c3eb9af95ad2183e9344f90e5688675d3c26329
Rozygeldiyew-git <rozygeldiyewudemy@gmail.com>
2022-11-09 10:39:38 +0500

    Something added by Rozygeldiyew

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.ynamdar.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.ynamdar.com',
+        port: '',
+        pathname: '/ynamdar/images/product/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
